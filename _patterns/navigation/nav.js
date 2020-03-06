15cbb9c20f44b2572203423d17074b885e502dfa
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-03-06 16:59:36 -0500

    Add javascript and jquery to filter navigation

---
+++ _patterns/navigation/nav.js
@@ -0,0 +1,9 @@
+$("#filters").on("click" , function() {
+  $(this).toggleClass(".filter-btn-active");
+  $("#filter-buttons").attr('hidden', function(_, attr){ return !attr});
+});
+
+$(".filter-btn").on("click" , function() {
+  $(this).toggleClass(".filter-btn-active");
+  $(this).siblings(".filter-links").attr('hidden', function(_, attr){ return !attr});
+});
--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@
 ---
 
 {% include_relative form-validation-helper.js %}
+{% include_relative jquery.js %}
 {% pattern_js %}
