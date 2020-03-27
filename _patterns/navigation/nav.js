1556abb43c707c6b9e1f62a5f3aa54437743da3c
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-03-27 14:17:35 -0400

    Add if statement to navigation javascript

--- _patterns/navigation/nav.js
+++ _patterns/navigation/nav.js
@@ -1,3 +1,9 @@
+var largeScreen = window.matchMedia('(min-width: 38em)');
+
+if (largeScreen.matches) {
+  $('#filters').click();
+}
+
 $("#filters").on("click" , function() {
   $(this).toggleClass("filter-btn-active");
   $("#filter-buttons").attr('hidden', function(_, attr){ return !attr});
